eaf1fde5668debf3f1c6a1f8fc260ea7b17da197
ZDmitrii11 <dima_zol@mail.com>
2020-12-07 12:56:04 +0200

    h

--- Script-files/script.js
+++ Script-files/script.js
@@ -1,36 +1,37 @@
+
+// DOM взяли все из Html
 const total = document.getElementById('total')
 const bill = document.getElementById('bill')
 const procents = document.getElementById('procent')
 const calcprocents = document.getElementById('calcprocent')
 const calctotal = document.getElementById('calctotal')
+
+
+// Функция расчета процента
 function procent_value() {
 
-
-
-    return Number(bill.value)/Number(procents.value)/100
+    return Number(bill.value)*Number(procents.value)/100
 }
 procent_value()
 
-
+// Функция разчета total
 function totalValue (){
 
-    return  (Number(bill.value)/Number(procents.value))/100+Number(bill.value)
+    return  (Number(bill.value)*Number(procents.value))/100+Number(bill.value)
 }
 
-let regex = /[0-9]/
-console.log(regex)
+// let regex = /[0-9]/
+// console.log(regex)
+
+// Добавляем Event к кнопке при клике выполняеются функции.
 total.addEventListener('click',()=>{
-    if (bill.value !== regex || procents.value !== regex ){
+
         calcprocents.value = `${procent_value()}`
         calcprocents.style.backgroundColor = 'green'
         calctotal.value = `${totalValue()}`
         calctotal.style.backgroundColor = 'green'
-    }
-    else if (bill.value ===NaN || procents.value ===NaN ) {
-        calcprocents.style.backgroundColor = 'red'
 
-        calctotal.style.backgroundColor = 'red'
 
-    }
+
 
 })
